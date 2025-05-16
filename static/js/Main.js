c466aecb417d77126868f84e65a52b2cd3dfe19c
BracketProto <creepertruck812@gmail.com>
2025-05-16 10:03:11 +0100

    Update Main.js

--- static/js/Main.js
+++ static/js/Main.js
@@ -1,4 +1,3 @@
-// DOM Elements
 const RAMElement = document.getElementById("RAM");
 const RAMCounter = document.getElementById("RAMCounter");
 const RAMIncreaseCounterParent = document.getElementById("RAMIncreaseParent");
